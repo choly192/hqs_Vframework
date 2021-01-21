4ddc1fe18d5fc5383eba350247e9352ac1c43e9e
choly192 <825391236@qq.com>
2021-01-21 18:49:03 +0800

    修改webpack配置的rules

--- webpackConfig/common/moduleRules.js
+++ webpackConfig/common/moduleRules.js
@@ -3,6 +3,7 @@
  */
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 提取css为一个单独的文件， 为每一个包含css的js文件创建一个css文件
 
+// eslint-disable-next-line no-undef
 module.exports = moduleRules = [
   { // 处理es6
     test: /\.js(x?)$/,
@@ -71,5 +72,54 @@ module.exports = moduleRules = [
         }
       }
     ]
-  }
+  },
+  { // 图片
+    test: /\.(png|jpg|gif|svg|ico)$/,
+    use: [
+      {
+        loader:'file-loader',
+        options:{
+          limit: 3072,
+          name: '[name].[ext]',
+          outputPath: './static/images' // 定义图片打包的输出位置
+        }
+      }
+    ]
+  },
+  { // 字体
+    test: /\.(eot|woff|woff2|ttf)$/i,
+    use: [
+      {
+        loader: 'url-loader',
+        options: {
+          limit: 3072,
+          name: '[name].[ext]',
+          outputPath: './static/fonts' // 定义字体输出位置
+        }
+      }
+    ]
+  },
+  { // 数据
+    test: /\.json$/,
+    use: [
+      {
+        loader: 'file-loader',
+        options: {
+          limit: 3072,
+          name: '[name].[ext]',
+          outputPath: './static/data'
+        }
+      }
+    ]
+  },
+  { // 音乐
+    test: /\.(mp3|mp4|wav)$/,
+    use: [
+      {
+        options: {
+          outputPath: './static/music' // 定义音乐输出位置
+        }
+      }
+    ]
+  },
 ];
